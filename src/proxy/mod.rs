2efe384c10064b7039fee0ed4e724680c0d11361
septech <septechx@proton.me>
2026-07-06 19:36:04 +0200

    feat: Basic packet forwarding proxy

--- src/proxy/mod.rs
+++ src/proxy/mod.rs
@@ -1,4 +1,8 @@
-use tracing::info;
+use std::net::SocketAddr;
+
+use tokio::io;
+use tokio::net::{TcpListener, TcpStream};
+use tracing::{error, info, trace, warn};
 
 use crate::plugin::{Event, EventBus, fire_event};
 
@@ -17,9 +21,45 @@ impl Proxy {
             Ok(self.event_bus.fire_event(env, Event::proxy_initialize()?))
         })?;
 
-        info!("Starting proxy on port {}", self.port);
+        trace!("Starting proxy on port {}", self.port);
+        let listener = TcpListener::bind(format!("127.0.0.1:{}", self.port)).await?;
+        info!("Started proxy on port {}", self.port);
 
-        Ok(())
+        loop {
+            let (socket, addr) = listener.accept().await?;
+            tokio::spawn(Self::handle_client(socket, addr));
+        }
+    }
+
+    async fn handle_client(socket: TcpStream, addr: SocketAddr) {
+        info!("Accepted connection from {}", addr);
+
+        let upstream = match TcpStream::connect("127.0.0.1:25566").await {
+            Ok(s) => s,
+            Err(e) => {
+                error!("Failed to connect to upstream: {e}");
+                return;
+            }
+        };
+
+        let (mut ri, mut wi) = socket.into_split();
+        let (mut ru, mut wu) = upstream.into_split();
+
+        let client_to_upstream = io::copy(&mut ri, &mut wu);
+        let upstream_to_client = io::copy(&mut ru, &mut wi);
+
+        tokio::select! {
+            r = client_to_upstream => {
+                if let Err(e) = r {
+                    warn!("client->upstream error: {e}");
+                }
+            }
+            r = upstream_to_client => {
+                if let Err(e) = r {
+                    warn!("upstream->client error: {e}");
+                }
+            }
+        }
     }
 }
 
@@ -27,6 +67,8 @@ impl Proxy {
 pub(crate) enum ProxyError {
     #[error("JNI error: {0}")]
     Jni(#[from] jni::errors::Error),
+    #[error("TCP error: {0}")]
+    Io(#[from] tokio::io::Error),
 }
 
 type Result<T> = std::result::Result<T, ProxyError>;
